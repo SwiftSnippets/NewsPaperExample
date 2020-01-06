1ff2089f45a2f435f7a189dc1610af31429cad20
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2020-01-06 04:35:41 -0800

    add project files

---
+++ NewsPaperExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsPaperExample
+//
+//  Created by Abdulsalam Mansour on 1/6/20.
+//  Copyright © 2020 abdulsalam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
